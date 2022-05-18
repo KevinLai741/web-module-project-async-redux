21f27d29afed897d221cd086cfa5436fac498dea
Kevin Lai <kevin.lai@gmail.com>
2022-05-18 16:20:36 -0400

    finished mvps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Activity from './components/Activity'
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <div className='intro'>  
+        <h1>Bored?</h1>
+        <h3>Here's a fun activity to do:</h3>
+      </div>
+      <Activity/>
     </div>
   );
 }
